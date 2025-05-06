0271377b88d8b9a194cb5cbefe6ad9f3458a186d
Paula Cadena <paulacadena@Mac.attlocal.net>
2025-05-05 19:43:44 -0500

    API endpoints preview (lint 3)

--- capp-connect/backend/ccserver/serializers.py
+++ capp-connect/backend/ccserver/serializers.py
@@ -1,47 +1,67 @@
-from .models import Tag, User, UserTag, Post, PostTag, Event, EventTag, Project, ProjectTag
 from rest_framework import serializers
+
+from .models import (
+    Event,
+    EventTag,
+    Post,
+    PostTag,
+    Project,
+    ProjectTag,
+    Tag,
+    User,
+    UserTag,
+)
+
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Tag
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class UserTagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserTag
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class PostTagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PostTag
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Event
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class EventTagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EventTag
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
-        fields = ['__all__']
+        fields = ["__all__"]
+
 
 class ProjectTagSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProjectTag
-        fields = ['__all__']
+        fields = ["__all__"]
