47d98a535ee96b53537ef3d654634b341ea9e338
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-06 12:10:26 -0500

    updated feed page and applied linter

--- capp-connect/frontend/app/(tabs)/feed.tsx
+++ capp-connect/frontend/app/(tabs)/feed.tsx
@@ -1,10 +1,13 @@
-import { RefreshControl, View } from "react-native";
+import { View } from "react-native";
 import FeedCard from "@/components/FeedCard";
 import { FlashList } from "@shopify/flash-list";
 
 const BODY = "The gentle hum of the city faded as the sun dipped below the skyline, casting long shadows across the quiet park. Leaves rustled in the evening breeze, swirling in small, playful circles around the old wooden bench. Somewhere nearby, laughter echoed-brief and bright-before dissolving into the soft chorus of distant traffic. In that moment, time seemed to slow, and the world paused to breathe, wrapped in the golden glow of twilight."
 
+
 export default function Feed() {
+
+
   return (
     <View style={{flex: 1}}>
 
@@ -14,10 +17,13 @@ export default function Feed() {
           }}
 
           data={Array(1000).fill("word")}
-          // estimatedItemSize={200}
+          estimatedItemSize={500}
+
+          // will use for loading more data
+          // onEndReached={() => {}}
+          // onEndReachedThreshold={}
+
         />
-
-
 
      </View>
   );
