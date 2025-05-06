ee01f521ca3cf55b7b7a77f44d93083ded9cb363
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-05 19:35:17 -0500

    created basic layout for feed

--- capp-connect/frontend/app/(tabs)/feed.tsx
+++ capp-connect/frontend/app/(tabs)/feed.tsx
@@ -1,22 +1,24 @@
-import { RefreshControl, ScrollView, Text } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import ProfilePhoto from '../../components/ProfilePhoto';
+import { RefreshControl, View } from "react-native";
+import FeedCard from "@/components/FeedCard";
+import { FlashList } from "@shopify/flash-list";
+
+const BODY = "The gentle hum of the city faded as the sun dipped below the skyline, casting long shadows across the quiet park. Leaves rustled in the evening breeze, swirling in small, playful circles around the old wooden bench. Somewhere nearby, laughter echoed-brief and bright-before dissolving into the soft chorus of distant traffic. In that moment, time seemed to slow, and the world paused to breathe, wrapped in the golden glow of twilight."
 
 export default function Feed() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+    <View style={{flex: 1}}>
 
-          <Text>This is the feed</Text>
+        <FlashList
+          renderItem={({item}) => {
+            return <FeedCard title={String(item)} body={String(BODY)} />
+          }}
+
+          data={Array(1000).fill("word")}
+          // estimatedItemSize={200}
+        />
 
 
-      </SafeAreaView>
-    </SafeAreaProvider>
+
+     </View>
   );
 }
