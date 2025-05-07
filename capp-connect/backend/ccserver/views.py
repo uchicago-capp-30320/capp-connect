a5c12d3f7afa94c41eb6e9e3cd2a8b1afb6774f5
Paula Cadena <paulacadena@Paulas-MacBook-Pro.local>
2025-05-07 18:13:05 -0500

    Posts API endpoint added (missing tables update)

--- capp-connect/backend/ccserver/views.py
+++ capp-connect/backend/ccserver/views.py
@@ -52,74 +52,8 @@ class GetPost(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-# class CreateContent(APIView):  # All types of content
-#     def post(self, request):
-#         serializer = ContentCreateSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class FetchContent(APIView):
-#     def get(self, request):
-#         n = int(request.query_params.get("n", 20))
-#         query = request.query_params.get("query", None)
-#         content_type = request.query_params.get("content_type", None)
-
-#         results = {"posts": [], "events": [], "projects": []}
-
-#         def get_ids(queryset, id_field):
-#             if query:
-#                 queryset = queryset.filter(title__icontains=query)
-#             return list(
-#                 queryset.order_by("-created_at")[:n].values_list(
-#                     id_field, flat=True
-#                 )
-#             )
-
-#         if content_type == "post":
-#             post_ids = get_ids(Post.objects.all(), "post_id")
-#             for post_id in post_ids:
-#                 post = Post.objects.get(post_id=post_id)
-#                 results["posts"].append(ContentInfoSerializer(post).data)
-#         elif content_type == "event":
-#             event_ids = get_ids(Event.objects.all(), "event_id")
-#             for event_id in event_ids:
-#                 event = Event.objects.get(event_id=event_id)
-#                 results["events"].append(ContentInfoSerializer(event).data)
-#         elif content_type == "project":
-#             project_ids = get_ids(Project.objects.all(), "project_id")
-#             for project_id in project_ids:
-#                 project = Project.objects.get(project_id=project_id)
-#                 results["projects"].append(ContentInfoSerializer(project).data)
-#         else:
-#             Response(
-#                 {
-#                     "error": "Invalid content type. Must be one of: post, event, project."
-#                 },
-#                 status=status.HTTP_400_BAD_REQUEST,
-#             )
-
-#         return Response(results, status=status.HTTP_200_OK)
-
-
-# class FetchContentInfo(APIView):
-#     def get(self, request):
-#         ids = request.query_params.getlist("ids")
-#         content = []
-
-#         for comp_id in ids:
-#             try:
-#                 model_name, pk = comp_id.split("_")
-#                 model = {"post": Post, "event": Event, "project": Project}[
-#                     model_name
-#                 ]
-#                 content.append(
-#                     ContentInfoSerializer(model.objects.get(pk=pk)).data
-#                 )
-#             except (KeyError, ValueError, model.DoesNotExist):
-#                 continue
-
-#         return Response(content, status=status.HTTP_200_OK)
+class GetAllPosts(APIView):
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
--- capp-connect/backend/connect/urls.py
+++ capp-connect/backend/connect/urls.py
@@ -22,22 +22,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("ccserver/<int:pk>/", views.GetProfile.as_view(), name="get_user"),
-    path("ccserver/post/<int:pk>/", views.GetPost.as_view(), name="get_post")
-    # path(
-    #     "ccserver/create_content/",
-    #     views.CreateContent.as_view(),
-    #     name="create_content",
-    # ),
-    # path(
-    #     "ccserver/fetch_content/",
-    #     views.FetchContent.as_view(),
-    #     name="fetch_content",
-    # ),
-    # path(
-    #     "ccserver/fetch_content_info/",
-    #     views.FetchContentInfo.as_view(),
-    #     name="fetch_content_info",
-    # ),
+    path("ccserver/post/<int:pk>/", views.GetPost.as_view(), name="get_post"),
+    path("ccserver/posts/", views.GetAllPosts.as_view(), name="get_all_posts")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
