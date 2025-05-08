73f56c34181571d501bb51d46ecb9417906c0a0e
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-07 19:02:14 -0500

    Integrated search bar to feed page with infinite scrolling cards

--- capp-connect/frontend/app/(tabs)/feed.tsx
+++ capp-connect/frontend/app/(tabs)/feed.tsx
@@ -1,25 +1,24 @@
-import { View,RefreshControl, ScrollView, Text } from "react-native";
+import { View } from "react-native";
 import FeedCard from "@/components/FeedCard";
 import { FlashList } from "@shopify/flash-list";
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import ProfilePhoto from '../../components/ProfilePhoto';
 import SearchBar from '../../components/SearchBar';
 
 const BODY = "The gentle hum of the city faded as the sun dipped below the skyline, casting long shadows across the quiet park. Leaves rustled in the evening breeze, swirling in small, playful circles around the old wooden bench. Somewhere nearby, laughter echoed-brief and bright-before dissolving into the soft chorus of distant traffic. In that moment, time seemed to slow, and the world paused to breathe, wrapped in the golden glow of twilight."
 
 export default function Feed() {
-
-
   return (
-    <View style={{flex: 1}}>
-
-        <SearchBar
+    <>
+      <SearchBar
           placeholder="Search..."
-          style={{ marginVertical: 5
-           }}
+          style={
+            { marginVertical: 10,
+              width: "90%",
+              alignSelf: "center"
+            }
+          }
           color="gray"
         />
-
+    <View style={{flex: 1, width:"100%"}}>
         <FlashList
           renderItem={({item}) => {
             return <FeedCard title={String(item)} body={String(BODY)} />
@@ -33,7 +32,7 @@ export default function Feed() {
           // onEndReachedThreshold={}
 
         />
-
      </View>
+     </>
   );
 }
