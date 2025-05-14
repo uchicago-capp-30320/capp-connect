89ee32647b66637e3bfdfe4a4072ce7ca6b0dcf4
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-14 12:09:04 -0500

    Updated _layout files to make use of color constants for consistency. The color constants should now persist across all pages. To apply them to individual components/text/etc. you still have to directly call the relevant colors

--- capp-connect/frontend/app/_layout.tsx
+++ capp-connect/frontend/app/_layout.tsx
@@ -1,13 +1,32 @@
 import { Stack } from 'expo-router/stack';
+import Colors from '@/themes/colors';
 
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ title: "Back", headerShown: false }} />
-      <Stack.Screen name="profile" options={{ title: "Profile"}} />
-      <Stack.Screen name="settings" options={{ title: "Settings"}} />
-      <Stack.Screen name="post" options={{ title: "Feed"}} />
+    <Stack screenOptions={{
+      contentStyle: {backgroundColor: Colors.background}
+    }}>
+      <Stack.Screen name="(tabs)" options={{ title: "Back", headerShown: false}} />
+      
+      <Stack.Screen name="profile" 
+        options={{ 
+          title: "Profile",
+          headerStyle: {backgroundColor: Colors.header},
+          headerTintColor: Colors.headerText
+        }} />
+      <Stack.Screen name="settings"
+        options={{ 
+          title: "Settings", 
+          headerStyle: {backgroundColor: Colors.header},
+          headerTintColor: Colors.headerText
+        }} />
+      <Stack.Screen name="post" 
+        options={{ 
+          title: "Feed",  
+          headerStyle: {backgroundColor: Colors.header},
+          headerTintColor: Colors.headerText
+        }} />
     </Stack>
   );
 }
