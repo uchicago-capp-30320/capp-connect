38db6ebf76d894b18bee36c24897f2c04f479f7d
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-14 12:07:17 -0500

    Created a themes folder with theming constants. Created a colors file that holds color constants. Created a skeleton for a containers constants file that should eventually encompass all relevant styling for relevant containers for consistency. It currently only applies the coloring

---
+++ capp-connect/frontend/themes/containers.ts
@@ -0,0 +1,16 @@
+import Colors from "./colors";
+import { StyleSheet } from "react-native";
+
+const Containers = StyleSheet.create({
+    cards: {
+        backgroundColor: Colors.cardBackground
+    },
+    buttons: {
+        backgroundColor: Colors.secondary
+    },
+    textFields: {
+        backgroundColor: Colors.textFieldBackground
+    }
+})
+
+export default Containers;
