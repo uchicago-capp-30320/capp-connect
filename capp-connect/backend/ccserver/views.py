5903810bf6973ece3f1b0f182399e70442077e8c
Paula Cadena <paulacadena@Paulas-MacBook-Pro.local>
2025-05-15 15:53:51 -0500

    profile endpoints - missing Auth verification

--- capp-connect/backend/ccserver/views.py
+++ capp-connect/backend/ccserver/views.py
@@ -2,19 +2,22 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# Create your views here.
 from .models import Post, Profile
-from .serializers import PostSerializer, ProfileSerializer
+from .serializers import (
+    PostSerializer,
+    ProfileListSerializer,
+    ProfileSerializer,
+)
 
 
 # tutorial: https://www.django-rest-framework.org/tutorial/3-class-based-views/
 
 
 class GetProfile(APIView):
-    def get(self, request, pk, format=None):
+    def get(self, request, username, format=None):
         try:
-            user = Profile.objects.get(pk=pk)
-            serializer = ProfileSerializer(user)
+            profile = Profile.objects.get(user__username=username)
+            serializer = ProfileSerializer(profile)
             return Response(serializer.data)
         except Profile.DoesNotExist:
             return Response(
@@ -22,12 +25,41 @@ class GetProfile(APIView):
                 status=status.HTTP_404_NOT_FOUND,
             )
 
-    def post(self, request, format=None):
-        serializer = ProfileSerializer(data=request.data)
+    def post(self, request, username, format=None):
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            return Response(
+                {"error": "Profile not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = ProfileSerializer(profile, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(
+        self, request, username, format=None
+    ):  # Missing permission check
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            return Response(
+                {"error": "Profile not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        profile.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class GetProfileList(APIView):
+    def get(self, request, format=None):
+        users = Profile.objects.all()
+        serializer = ProfileListSerializer(users, many=True)
+        return Response(serializer.data)
 
 
 class GetPost(APIView):
