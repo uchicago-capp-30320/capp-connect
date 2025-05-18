388f814904c3b8d65d5323fbfa9ca81b164f7945
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-17 21:54:33 -0500

    Used more explicit typing for the object/record in the cache

--- capp-connect/frontend/utils/caching.ts
+++ capp-connect/frontend/utils/caching.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Adapted from AsyncStorage documentation
 // https://react-native-async-storage.github.io/async-storage/docs/usage
 
-export const setCachedData = async (key: string, value: Object) => {
+export const setCachedData = async (key: string, value: Record<string, string>) => {
   try {
     const jsonValue = JSON.stringify(value);
     await AsyncStorage.setItem(key, jsonValue);
