f1b3da805e137c14596fa77d65a3c241a89245f7
bentoviml <leeor.bentovim@gmail.com>
2025-05-21 18:24:12 -0500

    BUG: missing import

--- capp-connect/slack_auth/cappconnect_auth/cappconnect_auth/urls.py
+++ capp-connect/slack_auth/cappconnect_auth/cappconnect_auth/urls.py
@@ -4,13 +4,14 @@ URL configuration for cappconnect_auth project.
 
 from allauth.socialaccount.providers.slack.views import oauth2_login
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Use AllAuth’s Slack login view
     path("auth/login/slack/", oauth2_login, name="slack_login"),
-    # AllAuth will now handle the callback internally — no need for your custom one
     path("accounts/", include("allauth.urls")),
+    # Temporary redirect so we know that this worked
+    path("", lambda request: HttpResponse("You're logged in via Slack!")),
 ]
