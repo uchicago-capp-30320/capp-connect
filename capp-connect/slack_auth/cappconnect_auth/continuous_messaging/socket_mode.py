12314accbcd3a0132130af53605c6f4cae3796b2
Kiran Jivnani <kiranjivnani@Kirans-MacBook-Pro.local>
2025-05-22 10:18:20 -0500

    added a func to get edits to messages and added in the documentation so I can go back to it for deleting msgs

--- capp-connect/slack_auth/cappconnect_auth/continuous_messaging/socket_mode.py
+++ capp-connect/slack_auth/cappconnect_auth/continuous_messaging/socket_mode.py
@@ -2,6 +2,13 @@ import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+#documentation so i don't forget: https://api.slack.com/tutorials/tracks/hello-world-bolt
+#https://docs.slack.dev/reference/events
+#https://tools.slack.dev/bolt-python/concepts/event-listening
+# for changing msg: https://api.slack.com/events/message/message_changed
+
+
+
 # No signing_secret — not needed for Socket Mode!
 app = App(
     token=os.environ["SLACK_BOT_TOKEN"],
@@ -12,13 +19,16 @@ app = App(
 
 @app.message()
 def get_msg(message, say):
-    type = message.get("type")
-    channel = message.get("channel")
-    subtype = message.get("subtype")
-    user = message.get("user")
-    text = message.get("text")
-    ts = message.get("ts") 
-    edited = message.get("edited")
+    type = message["type"]
+    channel = message["channel"]
+    user = message["user"]
+    text = message["text"]
+    ts = message["ts"]
+    edited = message.get("edited") #using .get just in case this is None. rest follow documentation. 
+    print("type:", type, "user:", user, "tect:", text, "channel:",channel, "ts:",ts, "if edited:",edited)
+   
+   
+    
 
 # {
 # 	"type": "message",
@@ -32,11 +42,51 @@ def get_msg(message, say):
 # 	}
 # }
 
-    print("type:", type, "user:", user, "tect:", text, "channel:",channel, "ts:",ts)
+
+@app.event("message") #https://api.slack.com/events/message/message_changed
+def record_changed_messages(body,logger):
+    event = body["event"]
+    if event["subtype"] == "message_changed": 
+        channel = event["channel"]
+        hidden = event["hidden"]
+        ts = event["ts"]
+        message = event.get("message", {})
+        edited = message.get("edited", {})
+        print("Edited text, time of edit, user who edited:", event["message"]["text"], event["message"]["edited"])
+
+ 
+    
+    
+
+
+#     {
+# 	"type": "message",
+# 	"subtype": "message_changed",
+# 	"hidden": true,
+# 	"channel": "C123ABC456",
+# 	"ts": "1358878755.000001",
+# 	"message": {
+# 		"type": "message",
+# 		"user": "U123ABC456",
+# 		"text": "Hello, world!",
+# 		"ts": "1355517523.000005",
+# 		"edited": {
+# 			"user": "U123ABC456",
+# 			"ts": "1358878755.000001"
+# 		}
+# 	}
+# }
+
+    
+ 
+
+
 
 if __name__ == "__main__":
     print("Starting Slack Socket Mode listener...")
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
+
 
 ### FOR NEXT STEPS: 
 
