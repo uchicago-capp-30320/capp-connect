75164fcb7966e56f9a18cc5b3a09f81ef081324c
paula-cadena <pcadena@uchicago.edu>
2025-05-24 17:50:23 -0500

    first change

--- capp-connect/backend/ccserver/views.py
+++ capp-connect/backend/ccserver/views.py
@@ -42,7 +42,8 @@ class GetProfile(APIView):
 
         serializer = ProfileSerializer(profile, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
+            if request.user != profile.user:
+                serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
