c1b2c22781370fb1255318bb2fa8d65334a146fc
paula-cadena <pcadena@uchicago.edu>
2025-05-25 11:29:12 -0500

    Slack Posts endpoints

--- capp-connect/backend/ccserver/views.py
+++ capp-connect/backend/ccserver/views.py
@@ -336,3 +336,42 @@ class MyProfileView(APIView):
         profile = Profile.objects.get(user=request.user)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+
+class SlackPost(APIView):
+    def get_object(self,ts,channel):
+        try:
+            return Post.objects.get(ts=ts,channel=channel)
+        except Post.DoesNotExist:
+            return None
+        
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request):
+        ts = request.data.get("ts")
+        channel = request.data.get("channel")
+        post = self.get_object(ts,channel)
+        if not post:
+            return Response(
+                {"error": "Post not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        ts = request.data.get("ts")
+        channel = request.data.get("channel")
+        post = self.get_object(ts,channel)
+        if post:
+            post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
