f0204cd1df49cbdf84fb397422bbeeaea3dd9a72
gregthemitch <gmitchelljr@uchicago.edu>
2025-05-26 20:36:15 -0500

    created metro.config.js to allow rich text editor to function per the documentation

---
+++ capp-connect/frontend/metro.config.js
@@ -0,0 +1,34 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+const webAliases = {
+  'react-native': 'react-native-web',
+  'react-native-webview': '@10play/react-native-web-webview',
+  'react-native/Libraries/Utilities/codegenNativeComponent':
+    '@10play/react-native-web-webview/shim',
+  'crypto': 'expo-crypto',
+};
+
+config.resolver.resolveRequest = (
+  context,
+  realModuleName,
+  platform,
+  moduleName
+) => {
+  if (platform === 'web') {
+    const alias = webAliases[realModuleName];
+    if (alias) {
+      return {
+        filePath: require.resolve(alias),
+        type: 'sourceFile',
+      };
+    }
+  }
+  return context.resolveRequest(context, realModuleName, platform, moduleName);
+};
+
+
+module.exports = config;
